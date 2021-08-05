0fb783d4feb65c5686d0075b0c89a6688a976a7e
coco <1399108257@qq.com>
2021-08-05 10:49:11 +0800

    update home

---
+++ src/components/home/home-welcome.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="home-welcome">
+    <h2>Welcome To Our Studio!</h2>
+    <h1>Tt's nice to meet you</h1>
+    <button id="home-btn">TELL ME MORE</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home-welcome",
+  data() {
+    return {};
+  },
+};
+</script>
+<style>
+#home-welcome {
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 55%;
+  transform: translate(-50%, -50%);
+}
+#home-welcome h2 {
+  font-family: "方正舒体";
+  font-size: 3rem;
+}
+#home-welcome h1 {
+  font-family: "Sansita-Black";
+  font-size: 4.7rem;
+}
+#home-btn {
+  margin: 10%;
+  width: 40%;
+  color: #444343;
+  font-size: 1.5rem;
+  font-weight: 700;
+  font-family: "Adobe 黑体 Std";
+  /* border-width: 0.05rem; */
+  opacity: 0.6;
+}
+@media screen and (max-width: 500px) {
+  #home-welcome {
+    text-align: center;
+    position: absolute;
+    top: 45%;
+    transform: translate(-50%, -50%);
+  }
+  #home-welcome h2 {
+    font-size: 2rem;
+  }
+  #home-welcome h1 {
+    font-size: 3rem;
+  }
+  #home-btn {
+    margin: 10%;
+    width: 40%;
+    color: #444343;
+    font-size: 0.5rem;
+    font-weight: 700;
+    font-family: "Adobe 黑体 Std";
+    opacity: 0.6;
+  }
+}
+</style>
