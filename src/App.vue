79ecb161c3b2d7043dd95d212da3658a35a8f673
coco <1399108257@qq.com>
2021-08-07 09:06:44 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,32 @@
   <div id="app">
     <NavBar></NavBar>
     <router-view></router-view>
+    <change-nav></change-nav>
   </div>
 </template>
 
 <script>
 //要驼峰命名哦
-import NavBar from './components/common/nav.vue'
+import NavBar from "./components/common/nav.vue";
+
 export default {
-  name: 'App',
-  components : {
-    NavBar
-  }
-}
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
 </script>
 
 <style>
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  html{
-    font-size : 14px;
-  }
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html {
+  font-size: 14px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'animate.css/animate.min.css'
+import '../src/assets/changeNav/download/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
